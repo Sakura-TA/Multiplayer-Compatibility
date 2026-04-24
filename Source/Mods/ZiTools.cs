d68d367948512cdbf8b82a037a3ca76e7a41f67e
Sakura-TA <Sakura_TA@163.com>
2026-04-25 01:15:16 +0800

    DontSync when adding Marks

--- Source/Mods/ZiTools.cs
+++ Source/Mods/ZiTools.cs
@@ -20,12 +20,17 @@ namespace Multiplayer.Compat
         static MethodInfo ListContainsMethod;
         static MethodInfo ListAddMethod;
         static MethodInfo ListRemoveMethod;
+
+        static FieldInfo DontSync;
+
         public ZiTools(ModContentPack mod)
         {
             LongEventHandler.ExecuteWhenFinished(LatePatch);
         }
         void LatePatch()
         {
+            DontSync = AccessTools.Field(AccessTools.TypeByName("Multiplayer.Client.Multiplayer"), "dontSync");
+
             var type = AccessTools.TypeByName("ZiTools.ZiTools_GameComponent");
             GetObjectsDatabase = AccessTools.Method(type, "GetObjectsDatabase");
 
@@ -47,7 +52,32 @@ namespace Multiplayer.Compat
             ListContainsMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Contains");
             ListAddMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Add");
             ListRemoveMethod = AccessTools.Method(typeof(List<>).MakeGenericType(type), "Remove");
+
+            type = AccessTools.TypeByName("ZiTools.MapMarksManager");
+            MpCompat.harmony.Patch(AccessTools.Method(type, "SetMarks"),
+                prefix: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Prefix)),
+                finalizer: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Finalizer))
+                );
+            MpCompat.harmony.Patch(AccessTools.Method(type, "RemoveMarks"),
+                prefix: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Prefix)),
+                finalizer: new HarmonyMethod(typeof(MapMarksManager_Marks_Patch), nameof(MapMarksManager_Marks_Patch.Finalizer))
+                );
         }
+
+        static class MapMarksManager_Marks_Patch
+        {
+            static object preDontSync;
+            public static void Prefix()
+            {
+                preDontSync = DontSync.GetValue(null);
+                DontSync.SetValue(null, true);
+            }
+            public static void Finalizer()
+            {
+                DontSync.SetValue(null, preDontSync);
+            }
+        }
+
 
         public static void PostDrawObjectsList(ref IExposable favChange)
         {
